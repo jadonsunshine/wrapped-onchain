3b20f631a033ce2539fd7d228e76e65b19426b59
Jadonamite <jadonamite@gmail.com>
2025-12-16 18:08:58 +0100

    Tweaking the designs for established UX patterns.

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "Remodeling from Base to Comp"
-git push -u origin main
-# git push -u origin Upgrade
+git commit -m "Tweaking the designs for established UX patterns."
+# git push -u origin main
+git push -u origin Tweaks
 echo "Pushed to remote repository."
